243559cc96ce907ad0ab26a191132884224074c2
nishantsisodiya <nishantsisodiya.softprodigy@gamil.com>
2023-04-05 18:59:35 +0530

    handelbars added

--- src/app.js
+++ src/app.js
@@ -2,13 +2,30 @@
 const express = require('express');
 const app = express();
 require('./db/connection')
+const path = require('path')
+const hbs = require('hbs')
+
+//path
+const static_path = path.join(__dirname , '../public');
+const template_path = path.join(__dirname , '../templates/views');
+const partials_path = path.join(__dirname , '../templates/partials');
 
 //port
-const port = process.env.port || 1080;
+const port = process.env.port || 3000;
+
+//Middleware
+app.use(express.static(static_path))
+app.set("view engine" , "hbs");
+app.set('views' , template_path);
+hbs.registerPartials(partials_path)
 
 //Routings
 app.get('/' , (req , res)=>{
-    res.send("Welcome to Home Page")
+    res.render('index')
+})
+
+app.get('/contact' , (req , res)=>{
+    res.render('contact')
 })
 
 
